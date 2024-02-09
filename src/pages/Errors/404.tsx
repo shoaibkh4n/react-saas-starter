d3f1ce694659872bd905c58218fb5eb0e6eaa665
shoaibkh4n <shoaibkhancomputer@gmail.com>
2024-02-09 23:51:48 +0530

    modified:   src/App.css 	new file:   src/pages/Errors/404.tsx

---
+++ src/pages/Errors/404.tsx
@@ -0,0 +1,21 @@
+function Notfound() {
+  return (
+    <>
+      <div className="err_page">
+        <div className="err_page_right">
+          <h1>404</h1>
+          <h4>OOPS. Looks like the page you're looking for no longer exists</h4>
+          <p>
+            Don't worry. Since you're valuable to us we will bring you back to
+            safety
+          </p>
+          <a href="/dashboard" className="err_btn">
+            Back to home
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Notfound;
